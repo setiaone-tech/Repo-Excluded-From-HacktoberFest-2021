f0a385316cc4d2e8f47320f1c5c685759ac479f9
hctn03 <91272124+hctn03@users.noreply.github.com>
2021-10-02 19:26:45 +0545

    Update random-user-gen.py

--- codes/random-user-gen.py
+++ codes/random-user-gen.py
@@ -1,5 +1,5 @@
 import requests
-url = 'https://randomuser.me/api/1.2/' # Add ?nat=NZ Or Any [Supported] Country Code For Specific Country 
+url = 'https://randomuser.me/api/1.2/' # Add ?nat=NZ Or Any [Supported] Country Code For Specific Country  
 details = requests.get(url)
 api = details.json()
 print(api)
